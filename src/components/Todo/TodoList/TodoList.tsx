1b2419986a72490e0849f95fbe437cc19461d967
Ashish Dungdung <official.ashishd2@gmail.com>
2024-11-12 20:32:08 +0530

    style(TodoListStatus): Implemented the mobile view style for TodoListStatus component

--- src/components/Todo/TodoList/TodoList.tsx
+++ src/components/Todo/TodoList/TodoList.tsx
@@ -2,11 +2,14 @@ import styles from "./TodoList.module.scss";
 
 // imports
 import TodoListItem from "./TodoListItem/TodoListItem";
+import TodoListStatus from "./TodoListStatus/TodoListStatus";
 
 const TodoList = function () {
   return (
     <div className={styles.todo_list}>
       <TodoListItem />
+
+      <TodoListStatus />
     </div>
   );
 };
--- src/components/Todo/TodoList/TodoListStatus/TodoListStatus.tsx
+++ src/components/Todo/TodoList/TodoListStatus/TodoListStatus.tsx
@@ -1,7 +1,12 @@
 import styles from "./TodoListStatus.module.scss";
 
 const TodoListStatus = function () {
-  return <div></div>;
+  return (
+    <div className={styles.todoListStatus}>
+      <p>5 items left</p>
+      <button>Clear Completed</button>
+    </div>
+  );
 };
 
 export default TodoListStatus;
