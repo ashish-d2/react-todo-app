67e152656b4c6a92380406dcc51505e5664aafaf
Ashish Dungdung <official.ashishd2@gmail.com>
2024-11-27 12:58:09 +0530

    style(TodoListItem, Header): Added Todo Completed styles - Added hover effect to moon icon in Header component - Added styles for todos which are marked completed.

--- src/components/Todo/TodoList/TodoListItem/TodoListItem.tsx
+++ src/components/Todo/TodoList/TodoListItem/TodoListItem.tsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
+
+// style import
 import styles from "./TodoListItem.module.scss";
 
 // imports
 import { ReactComponent as CrossIcon } from "./../../../../assets/images/icon-cross.svg";
+import { ReactComponent as CheckIcon } from "./../../../../assets/images/icon-check.svg";
+
+// In order to mark a todo completed - the btn style si completed_btn and text style is completed_text
 
 const TodoListItem = function () {
   return (
     <>
       <div className={styles.todoList_item}>
         <div className={styles.todoAndBtnContainer}>
-          <button className={styles.submit_btn}></button>
+          <button className={`${styles.submit_btn}`}>
+            <CheckIcon />
+          </button>
           <p>Jog around the park 3x</p>
         </div>
 
