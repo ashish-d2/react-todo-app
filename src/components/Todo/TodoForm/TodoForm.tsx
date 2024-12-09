855728e7018ffadd0381fb51d10665505c5a9076
Ashish Dungdung <official.ashishd2@gmail.com>
2024-12-09 20:51:55 +0530

    style(TodoForm): Dark mode for TodoForm - Added dark mode styles for todoForm component. - Added a 0.3s ease transition delay - Added to colors bg-form-dark and bg-form-dark-text.

--- src/components/Todo/TodoForm/TodoForm.tsx
+++ src/components/Todo/TodoForm/TodoForm.tsx
@@ -1,8 +1,19 @@
 import styles from "./TodoForm.module.scss";
 
+// importing context
+import ThemeContext from "../../../context/ThemeContext";
+import { useTheme } from "../../../context/ThemeContext";
+
 const TodoForm = function () {
+  const { theme } = useTheme(ThemeContext);
+
+  console.log(theme);
   return (
-    <form className={styles.todoForm}>
+    <form
+      className={`${styles.todoForm} ${
+        theme === "light" ? "" : styles.todoForm_dark
+      }`}
+    >
       <button type="submit" className={styles.submit_btn}></button>
       <input
         type="text"
